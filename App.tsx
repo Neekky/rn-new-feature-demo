53efa49ccfba6a3bca1b85349774cbf923f8b9e4
CengChao <chao1.zeng@aihuishou.com>
2022-07-03 12:48:49 +0800

    feat: 分包测试

--- App.tsx
+++ App.tsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, {lazy} from 'react';
 import 'react-native-gesture-handler';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {module1} from './route';
 const Stack = createNativeStackNavigator();
+
+// 分包测试
+const Test = lazy(() => import('./src/module2/test'));
 
 export default function App() {
   return (
@@ -16,6 +19,7 @@ export default function App() {
             options={routeInfo.options}
           />
         ))}
+        <Stack.Screen name={'Test'} component={Test} />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- metro.config.js
+++ metro.config.js
@@ -4,8 +4,22 @@
  *
  * @format
  */
+const Mcs = require('metro-code-split');
 
-module.exports = {
+// 拆包的配置
+const mcs = new Mcs({
+  output: {
+    // 配置你的 CDN 的 BaseURL
+    publicPath: 'https://static001.geekbang.org/resource/rn',
+  },
+  dll: {
+    entry: ['react-native', 'react'], // 要内置的 npm 库
+    referenceDir: './public/dll', // 拆包的路径
+  },
+  dynamicImports: {}, // dynamic import 是默认开启的
+});
+
+const busineConfig = {
   transformer: {
     getTransformOptions: async () => ({
       transform: {
@@ -15,3 +29,5 @@ module.exports = {
     }),
   },
 };
+
+module.exports = mcs.mergeTo(busineConfig);
